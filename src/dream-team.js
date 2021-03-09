9d75952e17b4a9fe9f6902d5796a6b8c150e426b
nekhaychik <ira.nekhaychik@gmail.com>
2021-03-09 20:23:11 +0300

    feat: add dream-team code

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(array) {
+  if (array === undefined || !Array.isArray(array) || array.length === 0)
+    return false;
+  let teamName = "";
+  let sortedteamName = [];
+  //filter - создаёт новый массив, определённый по условию, переданному в аргументе
+  sortedteamName = array.filter(el => typeof el == "string" && el.length != 0 ? true : false);
+  //map - перобразует массив по заданному условию, trim - убирает пробелы с двух сторон
+  sortedteamName = sortedteamName.map((el) => {return el.trim().toLowerCase();});
+  sortedteamName.sort();
+  sortedteamName.forEach(el => {
+    if(typeof el == 'string' && el.length != 0)
+      teamName += el.trim()[0].toUpperCase(); //добавление первого элемента в новый массив
+  });
+  return teamName;
 };
