4fda6d9abef4cf476d01665e29d9a9a92ab72cea
nekhaychik <ira.nekhaychik@gmail.com>
2021-03-14 20:39:11 +0300

    fix: extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,24 +1,31 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function repeater(str, options) {
-  let result = "";
-  if(!options.separator)
-    options.separator = '+';
-  if(!options.additionSeparator)
-    options.additionSeparator = '|';
 
-  if(!options.repeatTimes)
-    options.repeatTimes = 1;
-  if(!options.additionRepeatTimes)
-    options.additionRepeatTimes = 1;
+  str = String(str)
 
-  if(options.hasOwnProperty('addition')){
-    if(options.addition == null) options.addition = 'null';
-    else options.addition = options.addition.toString();
-    str += (options.addition + options.additionSeparator).repeat(options.additionRepeatTimes).slice(0,-options.additionSeparator.length);
+  if (!options.separator)
+      options.separator = '+';
+
+  if (!options.additionSeparator)
+      options.additionSeparator = '|';
+  
+  let additionBuffer = options.addition ? options.addition : '';
+  if (options.additionRepeatTimes) {
+      if (options.additionSeparator) {
+          additionBuffer = (options.addition + options.additionSeparator).repeat(options.additionRepeatTimes);
+          additionBuffer = additionBuffer.slice(0, additionBuffer.length - options.additionSeparator.length);
+      }
   }
-  str += options.separator;
-  result = str.repeat(options.repeatTimes).slice(0,-options.separator.length)
+
+  let result = str + (options.addition ? options.addition : '');
+  if (options.repeatTimes) {
+      if (options.separator) {
+          result = (str + additionBuffer + options.separator).repeat(options.repeatTimes);
+          result = result.slice(0, result.length - options.separator.length);
+      }
+  }
+
   return result;
 };
   
